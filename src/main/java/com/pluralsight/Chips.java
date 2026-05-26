d57235eb03cfe99928122036171a3b9645d6dc47
24rohitsamp <24rohitsamp@gmail.com>
2026-05-26 14:59:42 -0400

    Worked on the Chips Class

---
+++ src/main/java/com/pluralsight/Chips.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Chips {
+
+    private String type;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    public String getType() { return type; }
+
+    public double getPrice() { return 1.50; }
+
+    @Override
+    public String toString() {
+        return String.format("%s chips - $%.2f", type, getPrice());
+    }
+}
