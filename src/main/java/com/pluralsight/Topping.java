0650325546f014ffc36f954cf36f65a92bec1b17
24rohitsamp <24rohitsamp@gmail.com>
2026-05-26 14:42:27 -0400

    Added some comments for the Topping Class

--- src/main/java/com/pluralsight/Topping.java
+++ src/main/java/com/pluralsight/Topping.java
@@ -5,8 +5,8 @@ public class Topping {
     private boolean extra;
 
     public Topping(String name) {
-        this.name = name;
-        this.extra = false;
+        this.name = name; // name of topping
+        this.extra = false; // Whether or not the topping is extra price(Premium Topping)
     }
 
     public Topping(String name, boolean extra) {
@@ -14,9 +14,9 @@ public class Topping {
         this.extra = extra;
     }
 
-    public String getName() { return name; }
+    public String getName() { return name; } //getter
     public boolean isExtra() { return extra; }
-    public void setExtra(boolean extra) { this.extra = extra; }
+    public void setExtra(boolean extra) { this.extra = extra; } //setter
 
     public double getPrice(int sandwichSize) { return 0.0; } // Toppings are free by default (premium is added price)
 
