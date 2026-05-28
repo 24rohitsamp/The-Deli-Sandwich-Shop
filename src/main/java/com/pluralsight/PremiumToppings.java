7cf435e6553777f711de914aa56b139e2cf5dadc
24rohitsamp <24rohitsamp@gmail.com>
2026-05-27 23:29:08 -0400

    Worked on the code for Premium Topping (Abstract Class/ Parent)

---
+++ src/main/java/com/pluralsight/PremiumToppings.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public abstract class PremiumTopping extends Topping {
+
+    private boolean isExtra;
+
+    public PremiumTopping(String name, boolean isExtra) {
+        super(name);
+        this.isExtra = isExtra;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
+
+    public void setExtra(boolean extra) {
+        this.isExtra = extra;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + (isExtra ? " (extra)" : "");
+    }
+}
