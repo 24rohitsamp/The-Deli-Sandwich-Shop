e02ae9654b34b9b7f0637651ced109baf220d058
24rohitsamp <24rohitsamp@gmail.com>
2026-05-27 23:23:49 -0400

    Added some Comments on the code for HomeScreen Java Class

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -14,7 +14,7 @@ public class HomeScreen {
 
     public void display() {
         System.out.println("==========================================");
-        System.out.println("   Welcome to DELI-cious Sandwich Shop!  ");
+        System.out.println("   Welcome to Rohit's DELI-cious Sandwich Shop!  "); //message for users
         System.out.println("==========================================");
 
         boolean running = true;
@@ -23,7 +23,7 @@ public class HomeScreen {
             System.out.println("\n========== HOME SCREEN ==========");
             System.out.println("1) New Order");
             System.out.println("0) Exit");
-            System.out.print("Select an option: ");
+            System.out.print("Select an option: "); // 2 option Home Screen
 
             String choice = scanner.nextLine().trim();
 
@@ -34,10 +34,10 @@ public class HomeScreen {
                     break;
                 case "0":
                     System.out.println("\nThank you for visiting Rohit's DELI-cious Store! Goodbye!");
-                    running = false;
+                    running = false; // message when done with order
                     break;
                 default:
-                    System.out.println("Invalid option. Please enter 1 or 0.");
+                    System.out.println("Invalid option. Please enter 1 or 0."); // Have to enter 1 or 0 for order
             }
         }
 
